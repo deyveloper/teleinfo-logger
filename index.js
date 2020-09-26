a0840911dbc7f096e6f75e34a44fc1535d823247
Dav <davidsargsyandev@gmail.com>
2020-09-27 00:19:00 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Telegram } = require('telegraf');
+
+class TelegramErrorer {
+    logger;
+    bot;
+
+    constructor(logger, sendLevels, telegramToken, usersIds) {
+        this.logger = logger;
+        this.bot = new Telegram(telegramToken);
+        
+        this.logger
+            .on('data', (data) => {
+                if (sendLevels.levels.indexOf(data.level) != -1) {
+                    for (let i = 0; i < usersIds.length; i++) {
+                        this.bot.sendMessage(usersIds[i], `${data.level} ->\n${data.message}`);
+                    };
+                };
+            });
+    };
+};
+
+module.exports = TelegramErrorer;
